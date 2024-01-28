c6dc44c5e3e7a17acf61a51239c09d8e64ff6c1e
Nishant <nishantkumarjha.139@gmail.com>
2024-01-28 22:57:45 +0530

    initial commit

---
+++ JS/scripts.js
@@ -0,0 +1,8 @@
+$(document).ready(function(x) {
+  x.preventDefault();
+})
+
+
+// document.getElementById("btns_about").addEventListener("click", function() {
+// window.location.href="servey.html";});
+      
